241e2df85bb629920e8ae2ff6522ecedac92f791
Anna Rusakova <vdovina2196@mail.ru>
2022-04-28 16:33:10 +0300

    routes

--- routes/albumsRouter.js
+++ routes/albumsRouter.js
@@ -25,23 +25,26 @@ router.get('/addalbum', async (req, res) => {
 router.post('/addalbum', async (req, res) => { 
     try {
         await Album.create({ title: req.body.title, status: req.body.status, user_id: req.session.user.id}); ////??
-        return res.redirect('/albums'); // - после создания нового альбома делаем редирект на страничку этого альбома
+        return res.redirect('/albums'); // - после создания нового альбома делаем редирект на страничку со всеми альбомами
     } catch (error) {
         return res.send('Error');
     }
 });
 
+router.delete('/deletealbum', async (req, res) => { 
+    
+
 // /albums/:id - страница конкретного альбома
-router.get(':id', async (req, res) => { 
+router.get('/:id', async (req, res) => { 
     try {
         const photos = await Photo.findAll({
             where: { user_id: req.session.user.id, album_id: req.params.id } });
-            return res.render('myalbum', {photos});
+        const thisAlbum = await Album.findOne({where: {id: req.params.id}});
+        return res.render('album', {photos, thisAlbum});
         } catch (error) {
             return res.send('Error');
         }
     });
-
 
 module.exports = router
 
