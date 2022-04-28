a24ab621690712353d8b9a8696c845c03cc28203
Aleksandr Schepilov <gornostay4eg@mail.ru>
2022-04-28 16:31:17 +0300

    fetch for delete

---
+++ public/js/deletePhoto.js
@@ -0,0 +1,15 @@
+const bigContainer = document.querySelector('.bigContainer');
+
+bigContainer?.addEventListener('click', async (e) => {
+  e.preventDefault();
+  if (e.target.className === 'deletephoto') {
+    const parent = e.target.closest('[data-id]');
+    const { id } = parent.dataset;
+    const response = await fetch(`/photos/${id}`, {
+      method: 'DELETE',
+    });
+    if (response.ok) {
+      parent.remove();
+    }
+  }
+});
